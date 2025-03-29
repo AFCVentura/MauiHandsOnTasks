2fa49ac0b37640f7603635ed234e5bce625fe1eb
Ventura <ventura.py3@gmail.com>
2025-03-28 21:20:04 -0300

    feat: first commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HandsOnToDoList
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ViewModels/TodoItemViewModel.cs
@@ -0,0 +1,53 @@
+﻿using HandsOnToDoList.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace HandsOnToDoList.ViewModels
+{
+    public class TodoItemViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string nameTodoItem;
+        public string NameTodoItem
+        {
+            get => nameTodoItem;
+            set
+            {
+                nameTodoItem = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NameTodoItem)));
+            }
+        }
+
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+
+        public ICommand AddTodoItemCommand { get; }
+        public ICommand RemoveTodoItemCommand { get; }
+
+        public TodoItemViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            AddTodoItemCommand = new Command(AddTodoItem);
+            RemoveTodoItemCommand = new Command<TodoItem>(RemoveTodoItem);
+        }
+
+        private void AddTodoItem()
+        {
+            if (!string.IsNullOrWhiteSpace(NameTodoItem))
+            {
+                TodoItems.Add(new TodoItem { Name = NameTodoItem, Done = false });
+                NameTodoItem = string.Empty;
+            }
+        }
+
+        private void RemoveTodoItem(TodoItem todoItem)
+        {
+            TodoItems.Remove(todoItem);
+        }
+    }
+
+}
+
+
